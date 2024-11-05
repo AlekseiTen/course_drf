163da050c2d4880d8d7e2bd86ffdd9fd93ea6172
Aleksei Ten <amailcn@mail.ru>
2024-11-06 00:02:53 +0300

    создал  для урока - Generic-классы. Прописал урлы Создал сериализатор. Прописал урл до приложения courses

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 from rest_framework.viewsets import ModelViewSet
-
-from courses.models import Course
-from users.serializers import CourseSerializer
+from courses.models import Course, Lesson
+from courses.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonCreateApiView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListApiView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveApiView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateApiView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyApiView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
