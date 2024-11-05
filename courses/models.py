17d014a36e73e343056849646bd81aef411455ed
Aleksei Ten <amailcn@mail.ru>
2024-11-05 23:05:00 +0300

    создал новый проект на drf, модели для пользователя, курсов, и уроков, прописал настройки и зависимости

---
+++ courses/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    preview = models.ImageField(
+        upload_to="course/previews/", verbose_name="Фото", **NULLABLE
+    )
+    description = models.TextField(
+        verbose_name="Описание курса", **NULLABLE, help_text="Укажите описание курса"
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name="Название курса",
+        related_name="lessons",
+        **NULLABLE
+    )
+    title = models.CharField(max_length=200, verbose_name="Название урока")
+    description = models.TextField(verbose_name="Описание урока", **NULLABLE)
+    preview = models.ImageField(
+        upload_to="lesson/previews/", verbose_name="Фото", **NULLABLE
+    )
+    video_url = models.URLField(verbose_name="Ссылка на видео", **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
---
+++ users/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        **NULLABLE,
+        help_text="Введите номер телефона"
+    )
+    country = models.CharField(
+        max_length=150,
+        verbose_name="Страна",
+        **NULLABLE,
+        help_text="Введите страну проживания"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="Аватар",
+        **NULLABLE,
+        help_text="Загрузите свой аватар"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
