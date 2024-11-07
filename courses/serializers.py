760e194fd6381ff2eed2b431c8b8d1b16ad0557d
Aleksei Ten <amailcn@mail.ru>
2024-11-07 21:56:49 +0300

    создал модель Payment, в приложен users. В config указал url до приложения users

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from courses.models import Course, Lesson
 
+
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
@@ -10,6 +11,7 @@ class LessonSerializer(ModelSerializer):
 
 class CourseSerializer(ModelSerializer):
     """Для курса добавил поле количество уроков"""
+
     lessons_count = SerializerMethodField()
 
     def get_lessons_count(self, course):
@@ -18,4 +20,3 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "title", "lessons_count")
-
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from courses.models import Course, Lesson
+
 NULLABLE = {"blank": True, "null": True}
 
 
@@ -13,19 +15,19 @@ class User(AbstractUser):
         max_length=35,
         verbose_name="Телефон",
         **NULLABLE,
-        help_text="Введите номер телефона"
+        help_text="Введите номер телефона",
     )
     country = models.CharField(
         max_length=150,
         verbose_name="Страна",
         **NULLABLE,
-        help_text="Введите страну проживания"
+        help_text="Введите страну проживания",
     )
     avatar = models.ImageField(
         upload_to="users/avatars/",
         verbose_name="Аватар",
         **NULLABLE,
-        help_text="Загрузите свой аватар"
+        help_text="Загрузите свой аватар",
     )
 
     USERNAME_FIELD = "email"
@@ -37,3 +39,43 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ("cash", "Наличные"),
+        ("transfer", "Перевод на счет"),
+    ]
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        **NULLABLE,
+    )
+    payment_date = models.DateField(verbose_name="Дата оплаты")
+    paid_course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name="Оплаченный курс", **NULLABLE
+    )
+    separately_paid_lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        verbose_name="Отдельно оплаченный урок",
+        **NULLABLE,
+    )
+    payment_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Сумма оплаты",
+    )
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name="Способ оплаты",
+    )
+
+    def __str__(self):
+        return f"{self.user} - {self.payment_amount} - {self.payment_date}"
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
