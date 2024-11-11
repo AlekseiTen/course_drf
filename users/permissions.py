edcaac2b91e8264780d4648d2411db0d4b97eb60
Aleksei Ten <amailcn@mail.ru>
2024-11-12 01:01:07 +0300

    создал класс,  на проверку явл. ли пользов. владельцем

--- users/permissions.py
+++ users/permissions.py
@@ -10,4 +10,12 @@ class IsModer(permissions.BasePermission):
         return request.user.groups.filter(name="moders").exists()
 
 
+class IsOwner(permissions.BasePermission):
+    """
+    Проверяет, что пользователь является владельцем.
+    """
 
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
