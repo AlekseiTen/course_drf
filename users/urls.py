422ec0f1c9108d705da3f31350ab84d6a60479e9
Aleksei Ten <amailcn@mail.ru>
2024-11-11 21:38:52 +0300

    добавил возможность авторизованному пользователю просмотра курсов, а не авторизованному возможнось зарегистрироваться

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
@@ -11,12 +12,12 @@ urlpatterns = [
     path("register/", UserCreateAPIView.as_view(), name="register"),
     path(
         "login/",
-        TokenObtainPairView.as_view(),
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
         name="login",
     ),
     path(
         "token/refresh/",
-        TokenRefreshView.as_view(),
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
         name="token_refresh",
     ),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.generics import ListCreateAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny
+
 from users.filters import PaymentFilter
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
@@ -9,6 +11,7 @@ from users.serializers import PaymentSerializer, UserSerializer
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
